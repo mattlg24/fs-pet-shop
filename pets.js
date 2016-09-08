7cfa3ed4742d6fa049f855b0bcdc5405ab5c9fca
Matt Gardner <matthewlgardner@gmail.com>
2016-09-08 08:46:57 -0600

    Add index to read subcommand

--- pets.js
+++ pets.js
@@ -4,15 +4,26 @@ const fs = require('fs')
 const path = require('path')
 const petsPath = path.join(__dirname, 'pets.json')
 
-fs.readFile(petsPath, 'utf8', function(err, data) {
-  if (err) {
-    throw err
-  }
-  let pets = JSON.parse(data)
+let node = path.basename(process.argv[0])
+let file = path.basename(process.argv[1])
+let cmd = process.argv[2]
+let index = process.argv[3]
 
-  console.log(pets);
-})
 
-// console.error(`Usage: node pets.js [read | create | update | destroy]`)
-
-process.exit(1)
+if (cmd === 'read') {
+    fs.readFile(petsPath, 'utf8', function(err, data) {
+        if (err) {
+            throw err
+        }
+        let pets = JSON.parse(data)
+        if (index) {
+            console.log(pets[index])
+        } else {
+            console.log(pets);
+        }
+        // return process.stdout.write(JSON.stringify(pets))
+    })
+} else {
+    console.error(`Usage: ${node} ${path} read`)
+    process.exit(1)
+}
